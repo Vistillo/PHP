f00b371ffef580e3308183bca02682c516055c54
Vistillo <130785837+Vistillo@users.noreply.github.com>
2023-10-26 22:09:07 +0200

    Bucle FOR

---
+++ FOR.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Primer PHP</title>
+    </head>
+    <body>
+        <?php
+        for($x = 0; $x <= 5; $x++){
+            echo "El número es: $x <br>";
+        }
+        ?>
+    </body>
+</html>
