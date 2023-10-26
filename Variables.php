715cf057dd16faa9d53a4e016e2c4244e032d3ab
Vistillo <130785837+Vistillo@users.noreply.github.com>
2023-10-26 21:58:46 +0200

    Variables

---
+++ Variables.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Primer PHP</title>
+    </head>
+    <body>
+        <?php 
+        $txt = "Hola Mundo!";
+        $x = 5;
+        $y = 10.5;
+
+        echo $txt;
+        echo "<br>";
+        echo $x;
+        echo "<br>";
+        echo $y;
+        ?>
+    </body>
+</html>
