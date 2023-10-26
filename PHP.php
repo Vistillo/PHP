0a8c344ab1b8502ef93e779aebeac613bd700f5a
Vistillo <130785837+Vistillo@users.noreply.github.com>
2023-10-26 21:54:15 +0200

    Prueba PHP

---
+++ PHP.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Primer PHP</title>
+    </head>
+    <body>
+        <?php 
+        echo "Hola Paco campeón!";
+        ?>
+    </body>
+</html>
