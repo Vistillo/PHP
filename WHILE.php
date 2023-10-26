37aae998a340632a5dced979f7f36a2f004f94f6
Vistillo <130785837+Vistillo@users.noreply.github.com>
2023-10-26 22:05:41 +0200

    Bucle WHILE

---
+++ WHILE.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Primer PHP</title>
+    </head>
+    <body>
+        <?php 
+        $x = 1;
+
+        while($x <= 5){
+            echo "El valor de x es: ". $x ."<br>";
+            $x++;
+        }
+        ?>
+    </body>
+</html>
