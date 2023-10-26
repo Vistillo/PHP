720823b9beaaf19025a1d915cda52c7cd5f217d5
Vistillo <130785837+Vistillo@users.noreply.github.com>
2023-10-26 22:20:12 +0200

    Funcion

---
+++ Funcion.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Primer PHP</title>
+    </head>
+    <body>
+        <?php
+        function nombreCompleto($nombre){
+            echo "$nombre Molina.<br>";
+        }
+
+        nombreCompleto("Víctor");
+        nombreCompleto("Maribel");
+        nombreCompleto("David");
+        nombreCompleto("Carla");
+        ?>
+    </body>
+</html>
